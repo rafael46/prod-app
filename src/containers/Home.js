9a6751d680512706d2be5932398898a1c812fb89
Rafael Gdl <rafael@vipmarketingla.comcls>
2018-09-10 13:50:38 -0700

    setting up

---
+++ src/containers/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+// style = {text-align: left;}
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          {/* <h1></h1> */}
+          <p className = "left">h</p>
+        </div>
+      </div>
+    );
+  }
+}
+
