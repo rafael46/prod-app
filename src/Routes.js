a64b148471d406f6430458b5029f5c699b5fcf53
Rafael Gdl <rafael@vipmarketingla.comcls>
2018-09-12 14:06:46 -0700

    setting up

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import AppliedRoute from "./components/AppliedRoute"
 
-export default () =>
+
+export default ({childProps}) =>
   <Switch>
     <Route path="/" exact component={Home} />
     {/* new routes */}
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    {/* <AppliedRoute path="/login" exact component={Login} props={childProps} /> */}
     {/* Finally, catch all unmatched route */}
     <Route component = {NotFound}/>
   </Switch>;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import "./Home.css";
+import { API } from "aws-amplify";
 
 // style = {text-align: left;}
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isLoading: true,
+      contacts: []
+    };
+  }
+
+
+async componentDidMount(){
+
+  try {
+    const contacts = await this.contacts();
+    console.log(contacts);
+    this.setState({ contacts });
+  } catch (e) {
+    alert(e);
+  }
+
+  this.setState({ isLoading: false });
+
+}
+
+contacts(){
+  return API.get("contacts","/todos");
+}
 
   render() {
     return (
